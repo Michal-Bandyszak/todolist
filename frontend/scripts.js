80abe93489903a20dca8b49222e9998d8bcc4784
michal bandyszak <michalbandyszak@gmail.com>
2020-05-18 15:59:35 +0200

    new styles, ready fetch post && get

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,31 +1,38 @@
 const btnAdd = document.querySelector(".btn__add");
 const element = document.querySelector(".tasks__list");
 
+const showData = (data) => {
+	element.innerHTML = "";
+	data.forEach((data) => {
+		const li = document.createElement("li");
+		const btnDel = document.createElement("button");
+		li.innerHTML = data.task;
+		btnDel.innerText = "X";
+		element.appendChild(li);
+		element.appendChild(btnDel);
+	});
+}
+
+fetch('/todos')
+	.then((res) => res.json())
+	.then(showData)
+	.catch(error => console.error(error));
+
+
 document.forms.newTask.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch("/todo", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(
-      Object.fromEntries(new FormData(document.forms.newTask))
-    ),
-  })
-    .then(
-      fetch("/todos")
-        .then((res) => res.json())
-        .then((data) => {
-          element.innerHTML = "";
-          
-          data.forEach((data) => {
-            const li = document.createElement("li");
-            li.innerHTML = data.task;
-            element.appendChild(li);
-          });
-        })
-    )
-    .catch((error) => {
-      console.error(error);
-    });
+	e.preventDefault();
+	fetch("/todo", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(
+			Object.fromEntries(new FormData(document.forms.newTask))
+		),
+	})
+	.then((res) => res.json())
+	.then(showData)
+	.catch((error) => {
+		console.error(error);
+	});
 });
