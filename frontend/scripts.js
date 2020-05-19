741fe531ab5156b89f383c811368fffd626fd5ee
michal bandyszak <michalbandyszak@gmail.com>
2020-05-19 10:05:29 +0200

    my version without closures

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,5 +1,15 @@
 const btnAdd = document.querySelector(".btn__add");
 const element = document.querySelector(".tasks__list");
+const input_task = document.querySelector(".input__task")
+
+// const deleteData = (data) => {
+// 	fetch(`/todos/${data.id}`, {
+// 		method: "DELETE",
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 		}
+// 	}
+// )}
 
 const showData = (data) => {
 	element.innerHTML = "";
@@ -8,16 +18,27 @@ const showData = (data) => {
 		const btnDel = document.createElement("button");
 		li.innerHTML = data.task;
 		btnDel.innerText = "X";
-		element.appendChild(li);
-		element.appendChild(btnDel);
+		element.appendChild(li).id = data.id;
+		element.appendChild(btnDel).id = data.id;
+		input_task.value = ""
+		btnDel.addEventListener("click", fetch(`/todos/${li.id}`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				}
+			}
+			)
+		)
+		
+		console.log(btnDel.id)
 	});
 }
+
 
 fetch('/todos')
 	.then((res) => res.json())
 	.then(showData)
 	.catch(error => console.error(error));
-
 
 document.forms.newTask.addEventListener("submit", (e) => {
 	e.preventDefault();
