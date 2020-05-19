f8485b518862f906deac9bffbd34d220a8b390e8
bkuczynska <65155292+bkuczynska@users.noreply.github.com>
2020-05-19 21:46:28 +0200

    Update scripts.js

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -5,69 +5,82 @@ const tasksList = document.querySelector(".tasks__list");
 const inputTask = document.querySelector(".input__task");
 
 const handleError = (error) => {
-	console.error(error);
+  console.error(error);
 };
 
 const deleteData = (id = -1) =>
-	fetch(`/todo/${id}`, {
-		method: "DELETE",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-		.then((res) => res.json())
-		.catch(handleError);
+  fetch(`/todo/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .catch(handleError);
 
 const postData = (body) =>
-	fetch("/todo", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(body),
-	})
-		.then((res) => res.json())
-		.catch(handleError);
+  fetch("/todo", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .catch(handleError);
 
 const getData = () =>
-	fetch("/todos")
-		.then((res) => res.json())
-		.catch(handleError);
+  fetch("/todos")
+    .then((res) => res.json())
+    .catch(handleError);
 
 const getHTMLElement = (str) => {
-	const parser = new DOMParser();
-	const childNodes = parser.parseFromString(str, "text/html").body.childNodes;
-	return childNodes.length > 0 ? childNodes[0] : document.createElement("div");
+  const parser = new DOMParser();
+  const childNodes = parser.parseFromString(str, "text/html").body.childNodes;
+  return childNodes.length > 0 ? childNodes[0] : document.createElement("div");
 };
 
 const showData = (task) => {
-	tasksList.innerHTML = "";
-	inputTask.value = "";
+  tasksList.innerHTML = "";
+  inputTask.value = "";
 
-	task.forEach(({ task, id }) => {
-        const element = getHTMLElement(`
+  task.forEach(({ task, priority, id }) => {
+    const element = getHTMLElement(`
             <li>
                 <span>${task}</span>
                 <button class='delete_btn'>X</button>
             </li>
-        `);
+		`);
 
-        const deleteButton = element.querySelector("button");
+    /// PRIORITIES CLASS
+    const elementClass = element.classList;
+    switch (priority) {
+      case "low":
+        elementClass.add("low__priority");
+        break;
+      case "medium":
+        elementClass.add("medium__priority");
+        break;
+      case "high":
+        elementClass.add("high__priority");
+        break;
+      default:
+        elementClass.add("normal__priority");
+    }
 
-        deleteButton.addEventListener("click", () => {
-            console.log(id);
-            deleteData(id);
-        });
-        
-		tasksList.appendChild(element);
-	});
+    const deleteButton = element.querySelector("button");
+    deleteButton.addEventListener("click", () => {
+      deleteData(`${id}`);
+    });
+    tasksList.appendChild(element);
+  });
 };
 
 document.forms.newTask.addEventListener("submit", (e) => {
-	e.preventDefault();
-	const body = Object.fromEntries(new FormData(e.target));
-	const request = postData(body);
-	request.then(showData);
+  e.preventDefault();
+  const body = Object.fromEntries(new FormData(e.target));
+  const request = postData(body);
+  request.then(showData);
 });
 
 getData().then(showData);
