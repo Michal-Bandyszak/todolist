58002df79e51d7ca28c82a54648a63436300af47
michal bandyszak <michalbandyszak@gmail.com>
2020-05-19 12:57:42 +0200

    Cleaned code with old version of show data

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,68 +1,56 @@
 const btnAdd = document.querySelector(".btn__add");
-const tasksList = document.querySelector(".tasks__list");
-const inputTask = document.querySelector(".input__task");
-
-const stringToHTML = (str) => {
-		const parser = new DOMParser();
-		const doc = parser.parseFromString(str, 'text/html');
-		console.log(doc.body.)
-		return doc.body;
-	}
+const element = document.querySelector(".tasks__list");
+const input_task = document.querySelector(".input__task");
 
 const handleError = (error) => {
 	console.error(error)
 };
 
 const deleteData = (data) => {
-  console.log(data);
   fetch(`/todo/${data}`, {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
     },
-  }).then(getData).catch(handleError)
+  }).then(getData);
 };
 
-const postData = (body) => {
-	fetch("/todo", {
-		method: "POST",
-		headers: {
-		  "Content-Type": "application/json",
-		},
-		body: JSON.stringify(body),
-	  })
-		.then((res) => res.json())
-		.then(showData)
-		.catch(handleError)
-};
+// const showData = (data) => {
+//   element.innerHTML = "";
+//   data.forEach((data) => {
+//     const li = document.createElement("li");
+//     const btnDel = document.createElement("button");
+//     li.innerHTML = data.task;
+//     btnDel.innerText = "X";
+//     element.appendChild(li).id = data.id;
+//     element.appendChild(btnDel).id = data.id;
+//     input_task.value = "";
+//     btnDel.addEventListener("click", () => {
+//       deleteData(btnDel.id);
+//     });
+//   });
+// };
 
 const getData = () => {
   fetch("/todos")
-	.then((res) => res.json())
-	.then(showData)
-	.catch(handleError)
+    .then((res) => res.json())
+    .then(showData)
+    .catch(handleError);
 };
 
 
-const showData = (data) => {
-  tasksList.innerHTML = "";
-  inputTask.value = ""
-  data.forEach(({task, id }) => {
-	const element = stringToHTML(`
-	<li>
-		<span>${task}</span>
-		<button>X</button>
-	</li>
-`);
-    tasksList.appendChild(element.body).id = id;
-    // tasksList.appendChild(element.btnDel).id = data.id;
-    btnDel.addEventListener("click", () => {
-      deleteData(btnDel.id);
-    });
-  });
-};
 
-
+const postData = (body) => (
+    fetch("/todo", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+    })
+        .then((res) => res.json())
+        .catch(handleError)
+);
 
 document.forms.newTask.addEventListener("submit", (e) => {
 	e.preventDefault();
@@ -70,5 +58,3 @@ document.forms.newTask.addEventListener("submit", (e) => {
 	const request = postData(body);
 	request.then(showData);
 });
-
- getData().then(showData)
